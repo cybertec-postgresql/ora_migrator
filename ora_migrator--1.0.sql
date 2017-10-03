2b9e79f3ae0f3dc51a458cc66dc141cba082bf1d
Laurenz Albe <laurenz.albe@cybertec.at>
2017-10-03 10:53:46 +0200

    Rename "ora_table_cols" to "ora_columns", add "ora_tables"
    To simplify the query, drop the "primary_key" column from "ora_columns".
    Constraints will be handled in a different foreign table.

--- ora_migrator--1.0.sql
+++ ora_migrator--1.0.sql
@@ -10,32 +10,39 @@ CREATE FUNCTION create_oraviews (
 ) RETURNS void
    LANGUAGE plpgsql VOLATILE STRICT AS
 $$DECLARE
-   ora_table_cols_sql text := E'CREATE FOREIGN TABLE %I.ora_table_cols (\n'
-      '  schema varchar(128) NOT NULL,\n'
-      '  table_name varchar(128) NOT NULL,\n'
-      '  column_name varchar(128) NOT NULL,\n'
-      '  column_id integer NOT NULL,\n'
-      '  type_name varchar(128) NOT NULL,\n'
-      '  type_schema varchar(128) NOT NULL,\n'
-      '  length integer,\n'
-      '  precision integer,\n'
-      '  scale integer,\n'
-      '  nullable boolean NOT NULL,\n'
-      '  primary_key boolean NOT NULL\n'
+   ora_tables_sql text := E'CREATE FOREIGN TABLE %I.ora_tables (\n'
+      '   schema varchar(128) NOT NULL,\n'
+      '   table_name varchar(128) NOT NULL\n'
       ') SERVER %I OPTIONS (table ''('
-         'SELECT col.owner, col.table_name, col.column_name, col.column_id, col.data_type, col.data_type_owner,\n'
+         'SELECT owner, table_name\n'
+         'FROM all_tables\n'
+         'WHERE temporary = ''''N''''\n'
+         '  AND secondary = ''''N''''\n'
+         '  AND nested    = ''''NO''''\n'
+         '  AND dropped   = ''''NO'''''
+      ')'', max_long ''%s'', readonly ''true'')';
+
+   ora_columns_sql text := E'CREATE FOREIGN TABLE %I.ora_columns (\n'
+      '   schema varchar(128) NOT NULL,\n'
+      '   table_name varchar(128) NOT NULL,\n'
+      '   column_name varchar(128) NOT NULL,\n'
+      '   column_id integer NOT NULL,\n'
+      '   type_name varchar(128) NOT NULL,\n'
+      '   type_schema varchar(128) NOT NULL,\n'
+      '   length integer NOT NULL,\n'
+      '   precision integer,\n'
+      '   scale integer,\n'
+      '   nullable boolean NOT NULL\n'
+      ') SERVER %I OPTIONS (table ''('
+         'SELECT col.owner, col.table_name, col.column_name,\n'
+         '       col.column_id, col.data_type, col.data_type_owner,\n'
          '       col.char_length, col.data_precision, col.data_scale,\n'
-         '       CASE WHEN col.nullable = ''''Y'''' THEN 1 ELSE 0 END AS nullable,\n'
-         '       CASE WHEN primkey_col.position IS NOT NULL THEN 1 ELSE 0 END AS primary_key\n'
-         'FROM all_tab_columns col,\n'
-         '     (SELECT con.table_name, cons_col.column_name, cons_col.position\n'
-         '      FROM all_constraints con, all_cons_columns cons_col\n'
-         '      WHERE con.owner = cons_col.owner AND con.table_name = cons_col.table_name\n'
-         '        AND con.constraint_name = cons_col.constraint_name\n'
-         '        AND con.constraint_type = ''''P'''') primkey_col\n'
-         'WHERE col.table_name = primkey_col.table_name(+) AND col.column_name = primkey_col.column_name(+)'
+         '       CASE WHEN col.nullable = ''''Y'''' THEN 1 ELSE 0 END AS nullable\n'
+         'FROM all_tab_columns col'
       ')'', max_long ''%s'', readonly ''true'')';
 BEGIN
-   EXECUTE format('DROP FOREIGN TABLE IF EXISTS %I.ora_table_cols', schema);
-   EXECUTE format(ora_table_cols_sql, schema, server, max_viewdef);
+   EXECUTE format('DROP FOREIGN TABLE IF EXISTS %I.ora_tables', schema);
+   EXECUTE format(ora_tables_sql, schema, server, max_viewdef);
+   EXECUTE format('DROP FOREIGN TABLE IF EXISTS %I.ora_columns', schema);
+   EXECUTE format(ora_columns_sql, schema, server, max_viewdef);
 END;$$;
