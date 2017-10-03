1a3cbdf70337667bcd235074b7f0733abbdf24a9
Laurenz Albe <laurenz.albe@cybertec.at>
2017-10-03 11:35:16 +0200

    Add view "ora_check_constraints", improve formatting

--- ora_migrator--1.0.sql
+++ ora_migrator--1.0.sql
@@ -11,7 +11,7 @@ CREATE FUNCTION create_oraviews (
    LANGUAGE plpgsql VOLATILE STRICT AS
 $$DECLARE
    ora_tables_sql text := E'CREATE FOREIGN TABLE %I.ora_tables (\n'
-      '   schema varchar(128) NOT NULL,\n'
+      '   schema     varchar(128) NOT NULL,\n'
       '   table_name varchar(128) NOT NULL\n'
       ') SERVER %I OPTIONS (table ''('
          'SELECT owner, table_name\n'
@@ -23,26 +23,50 @@ $$DECLARE
       ')'', max_long ''%s'', readonly ''true'')';
 
    ora_columns_sql text := E'CREATE FOREIGN TABLE %I.ora_columns (\n'
-      '   schema varchar(128) NOT NULL,\n'
-      '   table_name varchar(128) NOT NULL,\n'
+      '   schema      varchar(128) NOT NULL,\n'
+      '   table_name  varchar(128) NOT NULL,\n'
       '   column_name varchar(128) NOT NULL,\n'
-      '   column_id integer NOT NULL,\n'
-      '   type_name varchar(128) NOT NULL,\n'
+      '   column_id   integer      NOT NULL,\n'
+      '   type_name   varchar(128) NOT NULL,\n'
       '   type_schema varchar(128) NOT NULL,\n'
-      '   length integer NOT NULL,\n'
-      '   precision integer,\n'
-      '   scale integer,\n'
-      '   nullable boolean NOT NULL\n'
+      '   length      integer      NOT NULL,\n'
+      '   precision   integer,\n'
+      '   scale       integer,\n'
+      '   nullable    boolean      NOT NULL\n'
       ') SERVER %I OPTIONS (table ''('
-         'SELECT col.owner, col.table_name, col.column_name,\n'
-         '       col.column_id, col.data_type, col.data_type_owner,\n'
-         '       col.char_length, col.data_precision, col.data_scale,\n'
-         '       CASE WHEN col.nullable = ''''Y'''' THEN 1 ELSE 0 END AS nullable\n'
-         'FROM all_tab_columns col'
+         'SELECT owner,       table_name,     column_name,\n'
+         '       column_id,   data_type,      data_type_owner,\n'
+         '       char_length, data_precision, data_scale,\n'
+         '       CASE WHEN nullable = ''''Y'''' THEN 1 ELSE 0 END AS nullable\n'
+         'FROM all_tab_columns'
+      ')'', max_long ''%s'', readonly ''true'')';
+
+   ora_check_constraints_sql text := E'CREATE FOREIGN TABLE %I.ora_check_constraints (\n'
+      '   schema          varchar(128) NOT NULL,\n'
+      '   table_name      varchar(128) NOT NULL,\n'
+      '   constraint_name varchar(128) NOT NULL,\n'
+      '   "deferrable"    boolean      NOT NULL,\n'
+      '   deferred        boolean      NOT NULL,\n'
+      '   condition       text         NOT NULL\n'
+      ') SERVER %I OPTIONS (table ''('
+         'SELECT owner, table_name, constraint_name,\n'
+         '       CASE WHEN deferrable = ''''DEFERRABLE'''' THEN 1 ELSE 0 END deferrable,\n'
+         '       CASE WHEN deferred   = ''''DEFERRED''''   THEN 1 ELSE 0 END deferred,\n'
+         '       search_condition\n'
+         'FROM all_constraints\n'
+         'WHERE constraint_type = ''''C''''\n'
+         '  AND status          = ''''ENABLED''''\n'
+         '  AND validated       = ''''VALIDATED''''\n'
+         '  AND invalid         IS NULL'
       ')'', max_long ''%s'', readonly ''true'')';
 BEGIN
+   /* ora_tables */
    EXECUTE format('DROP FOREIGN TABLE IF EXISTS %I.ora_tables', schema);
    EXECUTE format(ora_tables_sql, schema, server, max_viewdef);
+   /* ora_columns */
    EXECUTE format('DROP FOREIGN TABLE IF EXISTS %I.ora_columns', schema);
    EXECUTE format(ora_columns_sql, schema, server, max_viewdef);
+   /* ora_check_constraints */
+   EXECUTE format('DROP FOREIGN TABLE IF EXISTS %I.ora_check_constraints', schema);
+   EXECUTE format(ora_check_constraints_sql, schema, server, max_viewdef);
 END;$$;
